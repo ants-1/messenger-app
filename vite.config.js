b4e63cef1d5aa02b2b3e6b412034aa6b1f834c4a
Anthony <anthonysalisbury98@outlook.com>
2024-11-22 23:20:12 +0000

    Setup Tailwind and  shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
