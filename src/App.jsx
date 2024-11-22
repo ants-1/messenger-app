09bb52620c07aee09de7ae619460df3fcbe70555
Anthony <anthonysalisbury98@outlook.com>
2024-11-22 23:46:54 +0000

    Setup socket.io client

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+
+const socket = io.connect("http://localhost:3000");
 
 function App() {
+  function sendMessage() {
+    console.log("Button clicked");
+    socket.emit("send_message", { message: `Hello from ${socket.id}` });
+  }
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="text" placeholder="Message" />
+      <Button onClick={sendMessage}>Send</Button>
+    </div>
   );
 }
 
